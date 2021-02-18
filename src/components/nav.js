22c19bd15b231cd96075ee03dbf2658052875cfc
Shubham0850 <rajshubham0850@gmail.com>
2021-02-18 10:34:50 +0530

    shubham src

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./style.css";
 import logo from "../images/logo.png";
+import { useState } from "react";
 
 export default function Nav() {
+  const [close, setClose] = useState(true);
+
+  const onClose = () => {
+    setClose(true)
+  }
+
+  const onOpen = () => {
+    setClose(false)
+  }
+
   return (
     <nav className="navbar flex">
       {/* Navigation bar logo div */}
@@ -12,7 +23,13 @@ export default function Nav() {
         </a>
       </div>
       {/* Navigation bar component list  */}
-      <div className="nav-components">
+      <div className={`nav-components ${close ? "d-none" : "d-flex"}`}>
+        <div className="close-btn" onClick={onClose}>
+          <img
+            src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-close-512.png"
+            alt="close icon"
+          />
+        </div>
         <ul className="flex">
           <li>
             <a href="/">Home</a>
@@ -27,6 +44,13 @@ export default function Nav() {
             <a href="/contact">Contact Us</a>
           </li>
         </ul>
+      </div>
+      {/* Menu bar for mobile devices */}
+    <div className="menu-bar" onClick={onOpen}>
+        <img
+          src="https://img.icons8.com/carbon-copy/2x/menu.png"
+          alt="menu-bar"
+        />
       </div>
     </nav>
   );
